d48649e48f3f732fadf19bd0518fd348b111f681
Laurent Soria <nemsasse@gmail.com>
2022-05-22 16:31:44 +0200

    AJOUT FORM DE CONTACT

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function validateForm() {
+    let loc = document.getElementById("message");
+    let val = loc.value;
+    if (val == "") {
+      alert("YOU HAVE TO FILL YOUR MESSAGE !");
+    
+    }
+  }
